fcc40da6cf52472354f688bebfd28594c1a95219
もっちゃん <s7056i3056@gmail.com>
2024-03-16 23:57:34 +0900

    remove: 未使用のuseを削除

--- example-app/tests/Unit/UserTest.php
+++ example-app/tests/Unit/UserTest.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class UserTest extends TestCase
