8a99796c1004824c241cc0c942a73ee32424ee18
もっちゃん <s7056i3056@gmail.com>
2024-03-20 23:27:16 +0900

    test: joinの検証

--- example-app/tests/Unit/UserTest.php
+++ example-app/tests/Unit/UserTest.php
@@ -14,65 +14,81 @@ class UserTest extends TestCase
   /** @test */
   public function test()
   {
-      User::factory()->create();
+    User::factory()->create();
 
-      $users = DB::table('users')->selectRaw('count(*) as user_count')->get();
+    $users = DB::table('users')->selectRaw('count(*) as user_count')->get();
 
-      $this->assertSame($users->first()->user_count, 1);
+    $this->assertSame($users->first()->user_count, 1);
   }
 
   /** @test */
   public function join()
   {
-      User::factory()->has(Post::factory()->count(3))->create();
-      $result = DB::table('users')
-          ->join('posts', 'users.id', '=', 'posts.user_id')
-          ->select('users.name', 'posts.tweet', 'users.email')
-          ->get();
+    User::factory()->has(Post::factory()->count(3))->create();
+    $result = DB::table('users')
+      ->join('posts', 'users.id', '=', 'posts.user_id')
+      ->select('users.name', 'posts.tweet', 'users.email')
+      ->get();
   }
 
   /** @test */
   public function leftJoin()
   {
-      User::factory()->has(Post::factory()->count(2))->create();
-      $result = DB::table('users')
-          ->leftJoin('posts', 'users.id', '=', 'posts.user_id')
-          ->get();
+    User::factory()->has(Post::factory()->count(2))->create();
+    $result = DB::table('users')
+      ->leftJoin('posts', 'users.id', '=', 'posts.user_id')
+      ->get();
   }
 
   /** @test */
   public function rightJoin()
   {
-      User::factory()->has(Post::factory()->count(2))->create();
-      $result = DB::table('users')
-          ->rightJoin('posts', 'users.id', '=', 'posts.user_id')
-          ->get();
+    User::factory()->has(Post::factory()->count(2))->create();
+    $result = DB::table('users')
+      ->rightJoin('posts', 'users.id', '=', 'posts.user_id')
+      ->get();
   }
 
   /** @test */
   public function crossJoin()
   {
-      User::factory()->has(Post::factory()->count(2))->create();
+    User::factory()->has(Post::factory()->count(2))->create();
 
-      $result = DB::table('users')
-          ->crossJoin('posts')
-          ->get();
+    $result = DB::table('users')
+      ->crossJoin('posts')
+      ->get();
   }
 
   /** @test */
   public function joinClause()
   {
-      User::factory()->has(Post::factory())->create();
-      User::factory()->has(Post::factory())->create();
-      User::factory()->has(Post::factory())->create();
-      User::factory()->has(Post::factory())->create();
-      $result = DB::table('users')
-          ->join('posts', function (JoinClause $join) {
-              $join->on('users.id', '=', 'posts.user_id')
-                  ->where('posts.user_id', '>', 174);
-          })
-          ->get();
+    User::factory()->has(Post::factory())->create();
+    User::factory()->has(Post::factory())->create();
+    User::factory()->has(Post::factory())->create();
+    User::factory()->has(Post::factory())->create();
+    $result = DB::table('users')
+      ->join('posts', function (JoinClause $join) {
+        $join->on('users.id', '=', 'posts.user_id')
+          ->where('posts.user_id', '>', 174);
+      })
+      ->get();
+  }
 
-      dd($result);
+  /** @test */
+  public function joinSub()
+  {
+    User::factory()->has(Post::factory())->create();
+    User::factory()->has(Post::factory())->create();
+
+    $latestPosts = DB::table('posts')
+      ->select('user_id', DB::raw('MAX(created_at) as last_post_created_at'))
+      ->groupBy('user_id');
+
+    $users = DB::table('users')
+        ->joinSub($latestPosts, 'latests_posts', function (JoinClause $join) {
+            $join->on('users.id', '=', 'latests_posts.user_id');
+        })
+        ->get();
+    dd($users);
   }
 }
