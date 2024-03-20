473ac9ffc109776738c2d51c4d854c8407d693a8
もっちゃん <s7056i3056@gmail.com>
2024-03-20 17:30:07 +0900

    add: テストを追加

---
+++ example-app/tests/Unit/PostTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test() {
+        $user = User::factory()->has(Post::factory()->count(3))->create();
+
+        $this->assertSame($user->posts->first()->tweet, Post::first()->tweet);
+    }
+}
